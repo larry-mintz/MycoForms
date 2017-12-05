05235d4f51c29992abb197f189778ad6c88745de
Windows 7 <Windows 7@192.168.1.67>
2017-12-05 15:16:24 -0500

    Critical value for the Man Whitney U Test method calculated

--- src/main/java/MycologyFrame.java
+++ src/main/java/MycologyFrame.java
@@ -9893,6 +9893,21 @@ Remove(1);
           
          MW_testValue =  MW.mannWhitneyU(xdata, ydata);
           pvalue[11]= MW.mannWhitneyUTest(xdata, ydata);
+          NaturalRanking MWranked = new NaturalRanking(NaNStrategy.MINIMAL,TiesStrategy.MAXIMUM);
+          double[] MWXranks = MWranked.rank(xdata);
+          double addranks=0;
+          
+        
+          
+          
+          SummaryStatistics MWz =  new  SummaryStatistics();
+          for(int count=1;count<=xcount;++count)
+          addranks=+MWXranks[count];
+          if( xcount <=20  ||  ycount <=20  )
+           critical_value[13]=  (addranks-MWz.getMean()+.5) /MWz.getStandardDeviation();
+          else
+          critical_value[13]=    (xcount*ycount)/Math.sqrt(xcount*ycount*(xcount+ycount+1)/12);
+          
     }     
     
     
